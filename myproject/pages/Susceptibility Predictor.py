437a359bdf6f4f95acda7cb6263aa9b9e78387f2
SUPRAVO BISWAS <147735025+SupravoCoder@users.noreply.github.com>
2025-07-14 14:33:57 +0530

    Update Susceptibility Predictor.py

--- myproject/pages/Susceptibility Predictor.py
+++ myproject/pages/Susceptibility Predictor.py
@@ -118,7 +118,7 @@ MODELS_DIR = "."
 
 @st.cache_resource
 def load_resources():
-    model_path = os.path.join(MODELS_DIR, "EarthquakePredictor.pkl")
+    model_path = os.path.join(MODELS_DIR, "Susceptability_pred_ML/EarthquakePredictor.pkl")
     scaler_fd_path = os.path.join(MODELS_DIR, "fault_density_scaler.pkl")
     scaler_hd_path = os.path.join(MODELS_DIR, "hubdist_scaler.pkl")
     scaler_mag_path = os.path.join(MODELS_DIR, "mag_scaler.pkl")
